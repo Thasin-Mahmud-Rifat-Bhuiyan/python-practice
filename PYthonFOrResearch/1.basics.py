ceff6c9f5f2267e0eb5894812da232b3d01804ff
Thasin-Mahmud-Rifat-Bhuiyan <tmrifat03@gmail.com>
2026-05-06 02:17:31 +0600

    Add files via upload

---
+++ PYthonFOrResearch/1.basics.py
@@ -0,0 +1,15 @@
+name = "Thasin Mahmud Rifat Bhuiyan"
+id = 695
+
+print(name, end=" ")
+print(id)
+
+print(type(name))
+print(type(id))
+
+x = True
+y = False
+
+print(type(x))
+print(type(y))
+print(x == y)
